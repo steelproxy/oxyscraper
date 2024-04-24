897c7fcf53d29628b8c4f864c0871fe1057af1e9
Collin Rodes <cr271620@ohio.edu>
2024-04-24 00:56:08 -0400

    simplified output argument handling

--- oxylab_scraper.py
+++ oxylab_scraper.py
@@ -185,18 +185,16 @@ def main():
     )
     start = args.start or int(get_user_input("Enter page to start at", default=1))
     query = args.query or get_user_input("Enter query to search for")
-    if not args.output:
-        output_file_name = get_user_input(
+
+    if args.output != "none":
+        output_file_name = args.output or get_user_input(
             "Enter file to output to (optional)", default=None
         )
-    else:
-        output_file_name = args.output
-
-    if output_file_name != "none":
         try:
             output_file = open(output_file_name, "a")
             if output_file.closed:
                 print("Output file unable to be opened.")
+                sys.exit(1)
         except IOError:
             print("Error opening output file.")
             sys.exit(1)
